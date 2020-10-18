f1e7aba797c4215c53e99e6d8635e6127abcc6d6
Christopher Hui <christopherhui0719@gmail.com>
2020-10-18 21:38:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#Adding Numbers
+#int1 = 1
+#int2 = 3
+#var_text = "The answer is:"
+#print(var_text)
+#print(int1 + int2)
+
+#Group Project: Lesson 2
+
+
+#Lesson 2
+
+#Booleans
+#To declare a varlibe...
+#Night = True
+#Day = False
+
+#Lists
+
+location = ["At the school ", "At the park", "At the Apple Farm"]
+lists = ['Bar', 'Chips Ahoy', 'Sugur']
+l = len(lists)
+print(lists)
+
+#  .remove removes values from lists and   .extend adds to lists.
